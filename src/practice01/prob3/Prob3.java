e20cccbaea7622927ebf8e6995df3ddca583e06e
jungeuidon <mantopuma@naver.com>
2019-08-06 16:42:25 +0900

    3번문제 완료

--- src/practice01/prob3/Prob3.java
+++ src/practice01/prob3/Prob3.java
@@ -1,6 +1,28 @@
 package practice01.prob3;
 
+import java.util.Scanner;
+
 public class Prob3 {
 	public static void main (String args[]) {
+		Scanner scan = new Scanner(System.in);
+		System.out.print("숫자를 입력하세요 : ");
+		int num = scan.nextInt();
+		int sum =0;
+		
+		if(num%2==0) { //짝수
+			for (int i = 0; i <= num; i++) {
+				if(i%2==0) {
+					sum += i ;
+				}
+			}
+			System.out.print("결과값 : " + sum);
+		}else { //홀수
+			for (int i = 0; i <= num; i++) {
+				if(i%2!=0) {
+					sum += i ;
+				}
+			}
+			System.out.println("결과값 : " + sum);
+		}
 	}
 }
