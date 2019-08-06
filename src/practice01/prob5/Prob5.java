03886578ea4170801f4b514395bdf71b350132c4
jungeuidon <mantopuma@naver.com>
2019-08-06 19:53:27 +0900

    5번 완료

--- src/practice01/prob5/Prob5.java
+++ src/practice01/prob5/Prob5.java
@@ -2,5 +2,13 @@ package practice01.prob5;
 
 public class Prob5 {
 	public static void main (String args[]) {
+		
+		for (int i = 1; i < 100; i++) {
+			int gu = i % 10;
+			if(gu ==3 || gu==6 || gu==9) {
+				System.out.println(i + "짝");
+			}
+			
+		}
 	}
 }
