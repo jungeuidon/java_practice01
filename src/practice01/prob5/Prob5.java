2c1cd24e5f954635fdf0a557db0d5bf6fb919d05
jungeuidon <mantopuma@naver.com>
2019-08-07 15:16:42 +0900

    5번 수정완료(짝짝)

--- src/practice01/prob5/Prob5.java
+++ src/practice01/prob5/Prob5.java
@@ -4,11 +4,27 @@ public class Prob5 {
 	public static void main (String args[]) {
 		
 		for (int i = 1; i < 100; i++) {
-			int gu = i % 10;
-			if(gu ==3 || gu==6 || gu==9) {
+			int count =0;
+			int mok = i/10;
+			int na = i%10;
+			if (mok == 3 || mok == 6 || mok==9) {
+				count++;
+				if(na== 3 || na == 6 || na == 9) {
+					count++;
+				}
+				if(count==1) {
+					System.out.println(i + "짝");
+				}else if(count==2) {
+					System.out.println(i + "짝짝");
+				}
+				count=0;
+			}else if(na ==3 || na ==6 || na ==9) {
 				System.out.println(i + "짝");
+			}else {
+				System.out.println(i);
 			}
-			
 		}
 	}
+		
 }
+
